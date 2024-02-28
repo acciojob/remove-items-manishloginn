a3f57432e0c8deb5ef1429470f050a7f93dd773e
manishloginn <132683386+manishloginn@users.noreply.github.com>
2024-02-28 22:58:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+
+document
+  .querySelector("input[type=button]")
+  .addEventListener("click", function () {
+    const colorSelect = document.getElementById("colorSelect");
+    colorSelect.remove(colorSelect.selectedIndex);
+  });
